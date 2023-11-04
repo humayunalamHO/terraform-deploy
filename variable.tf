0c0c20fb2fdf1ff51bdf4e2c7487eb72d88040ca
humayun-alam <humayun.alam10@gmail.com>
2023-11-04 09:26:56 +0000

    github action

--- variable.tf
+++ variable.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "halam-eu-east1key"
+  default     = "h-terraform"
 }
 
 variable "instance_type" {
