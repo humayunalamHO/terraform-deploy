2c54e3678707af2559125b9fd761c9d0096593b1
humayun-alam <humayun.alam10@gmail.com>
2023-11-04 19:10:33 +0000

    github action

--- main.tf
+++ main.tf
@@ -42,22 +42,22 @@ resource "aws_instance" "ansible" {
   
  
 }
-#Create a new EC2 launch configuration
-resource "aws_instance" "ec2_private" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = ["${aws_security_group.webserver-security-group.id}"]
-  subnet_id                   = aws_subnet.private-subnet-1.id
-  associate_public_ip_address = false
-  #user_data                   = "${data.template_file.provision.rendered}"
-  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
-  lifecycle {
-    create_before_destroy = true
-  }
-  tags = {
-    "Name" = "EC2-PRIVATE"
-  }
- 
-}
+#resource "aws_instance" "ec2_private" {
+##Create a new EC2 launch configuration
+#  ami                         = var.ami_id
+#  instance_type               = var.instance_type
+#  key_name                    = var.key_name
+#  security_groups             = ["${aws_security_group.webserver-security-group.id}"]
+#  subnet_id                   = aws_subnet.private-subnet-1.id
+#  associate_public_ip_address = false
+#  #user_data                   = "${data.template_file.provision.rendered}"
+#  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
+#  lifecycle {
+#    create_before_destroy = true
+#  }
+#  tags = {
+#    "Name" = "EC2-PRIVATE"
+#  }
+# 
+#}
 ################################
