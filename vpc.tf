9a9d1b50bd7cac72b9df774c64e1e460aaf08210
humayun-alam <humayun.alam10@gmail.com>
2023-11-05 13:30:39 +0000

    github action

--- vpc.tf
+++ vpc.tf
@@ -79,6 +79,14 @@ resource "aws_security_group" "ssh-security-group" {
     cidr_blocks = ["0.0.0.0/0"]
  }
 
+  ingress {
+    description = "SSH Access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+ }
+
   egress {
     from_port   = 0
     to_port     = 0
