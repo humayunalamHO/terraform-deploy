00381c7a270b294b43e428b272f8aa7d3ea91acc
humayun-alam <humayun.alam10@gmail.com>
2023-11-05 15:36:14 +0000

    github action

--- main.tf
+++ main.tf
@@ -5,6 +5,38 @@ provider "aws" {
 }
 
 #Create a new EC2 launch configuration
+resource "aws_instance" "ec2_jenkins" {
+  ami                         = 	ami-0e054a15e68793cab
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  security_groups             = ["${aws_security_group.ssh-security-group.id}"]
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  associate_public_ip_address = true
+  #user_data                   = "${data.template_file.provision.rendered}"
+  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    "Name" = "EC2-JENKINS"
+  }
+
+resource "aws_instance" "ec2_prod" {
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  security_groups             = ["${aws_security_group.ssh-security-group.id}"]
+  subnet_id                   = aws_subnet.public-subnet-1.id
+  associate_public_ip_address = true
+  #user_data                   = "${data.template_file.provision.rendered}"
+  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    "Name" = "EC2-PROD"
+  }
+
 resource "aws_instance" "ec2_prod" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
