a0eea4301ea8282f0c14bbee21751b3c46a30c6b
humayun-alam <humayun.alam10@gmail.com>
2023-12-01 21:32:21 +0000

    github action

--- s3.tf
+++ s3.tf
@@ -5,7 +5,7 @@ provider "aws" {
 //Create s3 bucket
 resource "aws_s3_bucket" "s3-dev" {
   bucket = "my-artifact-upload"
-  region = var.aws_region
+  //region = var.aws_region
 
   tags = {
     Name        = "DevOps_bucket"
